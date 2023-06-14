19e3384b1812fe3bb62f980b7ce239ba44beab2a
tatsuki-kawai <tatsukitdu12133423@gmail.com>
2023-06-14 12:20:21 +0900

    '今までの変更'

--- PracticeSelenium.py
+++ PracticeSelenium.py
@@ -6,14 +6,20 @@ from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import chromedriver_binary
 import csv
+import os
 
 driver = webdriver.Chrome(ChromeDriverManager().install())
 yahoo_news_total_comments = []
 yahoo_news_page_comments = []
 page = 1
 
-URL = "https://news.yahoo.co.jp/articles/5b697b5a9fb5d8315b79f2ba0760c09c9acff872/comments"
-file_name = 'CSV/2023_03_02/america_taityuuseisaku.csv'
+URL = "https://news.yahoo.co.jp/articles/1fc57bba01d74ad314a332858cbf76c4ba745522/comments"
+folder_name = '2023_05_24'
+csv_file_name = '20230524_1.csv'
+
+folder_path = 'C:/Users/tatsuki/Desktop/program/TechnicalInvestigation/Data/yahoo/' + folder_name
+csv_file_path = 'C:/Users/tatsuki/Desktop/program/TechnicalInvestigation/Data/yahoo/' + folder_name + '/' + csv_file_name
+
 
 # 1ページ以外でのURL
 if not page == 1:
@@ -22,7 +28,7 @@ driver.get(URL)
 
 while(True):
     try:
-        yahoo_news_page_comments = driver.find_elements(By.CSS_SELECTOR, "p[class='UserCommentItem__Comment-eCuDbC ccpTzz']")
+        yahoo_news_page_comments = driver.find_elements(By.CSS_SELECTOR, "p[class='UserCommentItem__Comment-eoheEU cTIZmn']")
         print(driver.current_url)
         for c in yahoo_news_page_comments:
             print(c.text)
@@ -44,7 +50,11 @@ while(True):
     time.sleep(10)
 
 print(len(yahoo_news_total_comments))
-with open(file_name, 'w', newline='') as csv_file:
+
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+
+with open(csv_file_path, 'w', newline='') as csv_file:
     fieldnames = ['comment']
     writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
     writer.writeheader()
